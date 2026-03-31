9eb31018eec2154edb45a87761b09d47f8369c02
Jim Markunas <jimmarkunas@gmail.com>
2026-03-31 16:07:36 -0500

    NYL Fix 2

--- src/components/case-study/template/visuals/deferred/DeferredNylCarousel.tsx
+++ src/components/case-study/template/visuals/deferred/DeferredNylCarousel.tsx
@@ -1,22 +1,11 @@
 "use client"
 
-import { DeferredVisualLoader } from "@/components/case-study/template/visuals/deferred/DeferredVisualLoader"
-
-const loader = () => import("@/components/case-study/NylEditorialCarousel")
+import NylEditorialCarousel from "@/components/case-study/NylEditorialCarousel"
 
 type Props = {
   images: string[]
 }
 
 export function DeferredNylCarousel({ images }: Props) {
-  return (
-    <DeferredVisualLoader
-      cacheKey={`nyl-editorial-carousel-${images.length}`}
-      loader={loader}
-      componentProps={{ images }}
-      minHeightClassName="min-h-[240px] md:min-h-[400px]"
-      loadingLabel="Loading gallery..."
-      eager
-    />
-  )
+  return <NylEditorialCarousel images={images} />
 }
