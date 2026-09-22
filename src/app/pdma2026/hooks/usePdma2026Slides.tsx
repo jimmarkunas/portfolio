bfdfae6d680a84826252ad5059d8fabe003ca172
Jim Markunas <248513254+jimmarkunas@users.noreply.github.com>
2026-09-22 06:23:41 -0500

    fix(pdma): route Slide 6 to exact canonical reconstruction

--- src/app/pdma2026/hooks/usePdma2026Slides.tsx
+++ src/app/pdma2026/hooks/usePdma2026Slides.tsx
@@ -3,7 +3,8 @@ import type { Pdma2026Content, Pdma2026SlideKey } from "@/content/pdma2026";
 import { buildPdma2026SlideRegistry } from "../pdma2026SlideRegistry";
 import { Slide02 } from "../components/Slide02";
 import { Slide01, Slide03, Slide04, Slide05 } from "../components/Batch01Slides";
-import { Slide06, Slide07, Slide09, Slide10 } from "../components/CanonicalSlides06to10";
+import { Slide06 } from "../components/CanonicalSlide06Exact";
+import { Slide07, Slide09, Slide10 } from "../components/CanonicalSlides06to10";
 import { Slide08 } from "../components/CanonicalSlide08";
 import { Slide11, Slide12, Slide13, Slide14, Slide15 } from "../components/CanonicalSlides11to15";
 function renderSlide(key: Pdma2026SlideKey): ReactNode { switch (key) { case "slide-01": return <Slide01 />; case "slide-02": return <Slide02 />; case "slide-03": return <Slide03 />; case "slide-04": return <Slide04 />; case "slide-05": return <Slide05 />; case "slide-06": return <Slide06 />; case "slide-07": return <Slide07 />; case "slide-08": return <Slide08 />; case "slide-09": return <Slide09 />; case "slide-10": return <Slide10 />; case "slide-11": return <Slide11 />; case "slide-12": return <Slide12 />; case "slide-13": return <Slide13 />; case "slide-14": return <Slide14 />; case "slide-15": return <Slide15 />; default: return <div aria-hidden="true" />; } }
